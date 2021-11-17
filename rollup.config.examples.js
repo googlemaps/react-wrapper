2aee873e60e9f961dda7e0df4eb1364d5c3a76ed
Justin Poehnelt <jpoehnelt@google.com>
2021-11-17 13:50:26 -0700

    build: use separate tsconfig

--- rollup.config.examples.js
+++ rollup.config.examples.js
@@ -68,8 +68,7 @@ const template = ({attributes, files, meta, publicPath, title}) => {
 };
 
 const typescriptOptions = {
-  compilerOptions: {declaration: false, noEmit: true},
-  include: ['src/**/*', 'examples/**/*'],
+  tsconfig: 'tsconfig.examples.json',
 };
 
 const examples = fs
